54f43fd92f29eea7d26532c7049cdb6bc8dc2f33
Shimanta12 <sbemon1356@gmail.com>
2024-05-16 01:32:25 +0600

    first commit

---
+++ Module 1/data_types.js
@@ -0,0 +1,23 @@
+// Primitive data types(copied by value);
+
+let id = 10; // number
+let name = "Shimanto"; // string
+let cgpa = 3.5; // number
+let married = false; // boolean
+let fate = undefined; // undefined
+let savings = null; // object :(
+
+
+// Non primitive(copied by reference)
+let friends = ["Safwan", "Banna", "Arman", "Rafi", "Sakib", "Rayhan", "Shakib bhai", "Anik", "Wasit", "Proyash"]; // object
+
+let skills = {
+    "language" : ["C", "C++", "JavaScript", "Python"],
+    "hobbies" : ["Gaming", "Football", "Travelling"] 
+};
+
+
+console.log(id, name, cgpa, married, fate, savings);
+
+console.log(skills);
+console.log(typeof(skills));
